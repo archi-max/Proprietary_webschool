9651c9cf07bc3c8d8c1ceee2ce0e1db474d5c1f9
PythonicNoob <okansht@gmail.com>
2022-02-07 11:35:56 +0530

    added password change.

--- user/urls.py
+++ user/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('logout/', views.UserLogoutView.as_view(next_page="/login"), name='logout'),
     path('users/export/', views.export_users, name='user_list'),
     path('profile/', views.UserUpdateView.as_view(), name='profile'),
-    path('changepassword/', auth_views.PasswordChangeView.as_view(), name='changepassword'),
+    path('changepassword/', auth_views.PasswordChangeView.as_view(template_name="backend/form_error.html", success_url="/formsuccess/"), name='changepassword'),
 ]
