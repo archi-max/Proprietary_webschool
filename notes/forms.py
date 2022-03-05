42ca5954b89236bcd01a10f0cc6c1108d7192022
PythonicNoob <okansht@gmail.com>
2022-03-06 01:01:47 +0530

    Removed old notion solution function

--- notes/forms.py
+++ notes/forms.py
@@ -38,14 +38,6 @@ def create_notion_page(dbid, title="Notebook Title", description="Notebook Descr
     print(result)
     return result['id'], result['url']
 
-def create_fake_notion_page(title="Notebook Title"):
-    url = 'http://localhost:8080/pages/'
-    data = {"blocks": [{"tag": "h1", "html": title, "imageUrl": ""},{"tag": "p", "html": "", "imageUrl": ""}]}
-    headers = {"Content-Type": "application/json"}
-    res = requests.post(url, json=data, headers=headers)
-    res = json.loads(res.text)
-    return res['pageId']
-
 
 class NotebookForm(forms.ModelForm):
 
